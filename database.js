ca2cbc96608f29204b82dfe0a44678cbafdd23d1
Alejandro Alvarez <alejandro.auribe1@gmail.com>
2022-11-23 16:45:52 -0500

    Commit con archivo Javascript y modificaciones funcionales

---
+++ database.js
@@ -0,0 +1,151 @@
+const botonGuardar = document.getElementById('botonGuardar');
+
+botonGuardar.addEventListener('click', guardarInformacionApuesta);
+
+
+
+function guardarInformacionApuesta() {
+    
+    const nombreApostador = document.getElementById('nombreApostador').value;
+    const resultadoIL = parseInt(document.getElementById('resultadoIL').value);
+    const resultadoIIL = parseInt(document.getElementById('resultadoIIL').value);
+    const resultadoIIIL = parseInt(document.getElementById('resultadoIIIL').value);
+    const resultadoIVL = parseInt(document.getElementById('resultadoIVL').value);
+    const resultadoVL = parseInt(document.getElementById('resultadoVL').value);
+    const resultadoVIL = parseInt(document.getElementById('resultadoVIL').value);
+    const resultadoVIIL = parseInt(document.getElementById('resultadoVIIL').value);
+    const resultadoVIIIL = parseInt(document.getElementById('resultadoVIIIL').value);
+    const resultadoIXL = parseInt(document.getElementById('resultadoIXL').value);
+    const resultadoXL = parseInt(document.getElementById('resultadoXL').value);
+    const resultadoXIL = parseInt(document.getElementById('resultadoXIL').value);
+    const resultadoXIIL = parseInt(document.getElementById('resultadoXIIL').value);
+    const resultadoXIIIL = parseInt(document.getElementById('resultadoXIIIL').value);
+    const resultadoXIVL = parseInt(document.getElementById('resultadoXIVL').value);
+    const resultadoXVL = parseInt(document.getElementById('resultadoXVL').value);
+    const resultadoXVIL = parseInt(document.getElementById('resultadoXVIL').value);
+    const resultadoXVIIL = parseInt(document.getElementById('resultadoXVIIL').value);
+    const resultadoXVIIIL = parseInt(document.getElementById('resultadoXVIIIL').value);
+    const resultadoXIXL = parseInt(document.getElementById('resultadoXIXL').value);
+    const resultadoXXL = parseInt(document.getElementById('resultadoXXL').value);
+    const resultadoXXIL = parseInt(document.getElementById('resultadoXXIL').value);
+    const resultadoXXIIL = parseInt(document.getElementById('resultadoXXIIL').value);
+    const resultadoXXIIIL = parseInt(document.getElementById('resultadoXXIIIL').value);
+    const resultadoXXIVL = parseInt(document.getElementById('resultadoXXIVL').value);
+    const resultadoXXVL = parseInt(document.getElementById('resultadoXXVL').value);
+    const resultadoXXVIL = parseInt(document.getElementById('resultadoXXVIL').value);
+    const resultadoXXVIIL = parseInt(document.getElementById('resultadoXXVIIL').value);
+    const resultadoXXVIIIL = parseInt(document.getElementById('resultadoXXVIIIL').value);
+    const resultadoXXIXL = parseInt(document.getElementById('resultadoXXIXL').value);
+    const resultadoXXXL = parseInt(document.getElementById('resultadoXXXL').value);
+    const resultadoXXXIL = parseInt(document.getElementById('resultadoXXXIL').value);
+    const resultadoXXXIIL = parseInt(document.getElementById('resultadoXXXIIL').value);
+    const resultadoXXXIIIL = parseInt(document.getElementById('resultadoXXXIIIL').value);
+    const resultadoXXXIVL = parseInt(document.getElementById('resultadoXXXIVL').value);
+    const resultadoXXXVL = parseInt(document.getElementById('resultadoXXXVL').value);
+    const resultadoXXXVIL = parseInt(document.getElementById('resultadoLXXXVI').value);
+    const resultadoXXXVIIL = parseInt(document.getElementById('resultadoXXXVIIL').value);
+    const resultadoXXXVIIIL = parseInt(document.getElementById('resultadoXXXVIIIL').value);
+    const resultadoXXXIXL = parseInt(document.getElementById('resultadoXXXVIXL').value);
+    const resultadoXLL = parseInt(document.getElementById('resultadoXLL').value);
+    const resultadoXLIL = parseInt(document.getElementById('resultadoXLIL').value);
+    const resultadoXLIIL = parseInt(document.getElementById('resultadoXLIIL').value);
+    const resultadoXLIIIL = parseInt(document.getElementById('resultadoXLIIIL').value);
+    const resultadoXLIVL = parseInt(document.getElementById('resultadoXLIVL').value);
+    const resultadoXLVL = parseInt(document.getElementById('resultadoXLVL').value);
+    const resultadoXLVIL = parseInt(document.getElementById('resultadoXLVIL').value);
+    const resultadoXLVIIL = parseInt(document.getElementById('resultadoXLVIIL').value);
+    const resultadoXLVIIIL = parseInt(document.getElementById('resultadoXLVIIIL').value);
+
+    const resultadoIR = parseInt(document.getElementById('resultadoIR').value);
+    const resultadoIIR = parseInt(document.getElementById('resultadoIIR').value);
+    const resultadoIIIR = parseInt(document.getElementById('resultadoIIIR').value);
+    const resultadoIVR = parseInt(document.getElementById('resultadoIVR').value);
+    const resultadoVR = parseInt(document.getElementById('resultadoVR').value);
+    const resultadoVIR = parseInt(document.getElementById('resultadoVIR').value);
+    const resultadoVIIR = parseInt(document.getElementById('resultadoVIIR').value);
+    const resultadoVIIIR = parseInt(document.getElementById('resultadoVIIIR').value);
+    const resultadoIXR = parseInt(document.getElementById('resultadoIXR').value);
+    const resultadoXR = parseInt(document.getElementById('resultadoXR').value);
+    const resultadoXIR = parseInt(document.getElementById('resultadoXIR').value);
+    const resultadoXIIR = parseInt(document.getElementById('resultadoXIIR').value);
+    const resultadoXIIIR = parseInt(document.getElementById('resultadoXIIIR').value);
+    const resultadoXIVR = parseInt(document.getElementById('resultadoXIVR').value);
+    const resultadoXVR = parseInt(document.getElementById('resultadoXVR').value);
+    const resultadoXVIR = parseInt(document.getElementById('resultadoXVIR').value);
+    const resultadoXVIIR = parseInt(document.getElementById('resultadoXVIIR').value);
+    const resultadoXVIIIR = parseInt(document.getElementById('resultadoXVIIIR').value);
+    const resultadoXIXR = parseInt(document.getElementById('resultadoXIXR').value);
+    const resultadoXXR = parseInt(document.getElementById('resultadoXXR').value);
+    const resultadoXXIR = parseInt(document.getElementById('resultadoXXIR').value);
+    const resultadoXXIIR = parseInt(document.getElementById('resultadoXXIIR').value);
+    const resultadoXXIIIR = parseInt(document.getElementById('resultadoXXIIIR').value);
+    const resultadoXXIVR = parseInt(document.getElementById('resultadoXXIVR').value);
+    const resultadoXXVR = parseInt(document.getElementById('resultadoXXVR').value);
+    const resultadoXXVIR = parseInt(document.getElementById('resultadoXXVIR').value);
+    const resultadoXXVIIR = parseInt(document.getElementById('resultadoXXVIIR').value);
+    const resultadoXXVIIIR = parseInt(document.getElementById('resultadoXXVIIIR').value);
+    const resultadoXXIXR = parseInt(document.getElementById('resultadoXXIXR').value);
+    const resultadoXXXR = parseInt(document.getElementById('resultadoXXXR').value);
+    const resultadoXXXIR = parseInt(document.getElementById('resultadoXXXIR').value);
+    const resultadoXXXIIR = parseInt(document.getElementById('resultadoXXXIIR').value);
+    const resultadoXXXIIIR = parseInt(document.getElementById('resultadoXXXIIIR').value);
+    const resultadoXXXIVR = parseInt(document.getElementById('resultadoXXXIVR').value);
+    const resultadoXXXVR = parseInt(document.getElementById('resultadoXXXVR').value);
+    const resultadoXXXVIR = parseInt(document.getElementById('resultadoXXXVIR').value);
+    const resultadoXXXVIIR = parseInt(document.getElementById('resultadoXXXVIIR').value);
+    const resultadoXXXVIIIR = parseInt(document.getElementById('resultadoRXXXVIII').value);
+    const resultadoXXXIXR = parseInt(document.getElementById('resultadoXXXIXR').value);
+    const resultadoXLR = parseInt(document.getElementById('resultadoXLR').value);
+    const resultadoXLIR = parseInt(document.getElementById('resultadoXLIR').value);
+    const resultadoXLIIR = parseInt(document.getElementById('resultadoXLIIR').value);
+    const resultadoXLIIIR = parseInt(document.getElementById('resultadoXLIIIR').value);
+    const resultadoXLIVR = parseInt(document.getElementById('resultadoXLIVR').value);
+    const resultadoXLVR = parseInt(document.getElementById('resultadoXLVR').value);
+    const resultadoXLVIR = parseInt(document.getElementById('resultadoXLVIR').value);
+    const resultadoXLVIIR = parseInt(document.getElementById('resultadoXLVIIR').value);
+    const resultadoXLVIIIR = parseInt(document.getElementById('resultadoXLVIIIR').value);
+
+        const informacionApuesta = [];
+        
+        informacionApuesta.push ({
+        
+            Nombre: nombreApostador, Resultados: [[resultadoIL,resultadoIR], [resultadoIIL,resultadoIIR], [resultadoIIIL,resultadoIIIR], [resultadoIVL,resultadoIVR], [resultadoVL,resultadoVR], [resultadoVIL,resultadoVIR], [resultadoVIIL,resultadoVIIR], [resultadoVIIIL,resultadoVIIIR], [resultadoIXL,resultadoIXR],
+            [resultadoXL,resultadoXR], [resultadoXIL,resultadoXIR], [resultadoXIIL,resultadoXIIR], [resultadoXIIIL,resultadoXIIIR], [resultadoXIVL,resultadoXIVR], [resultadoXVL,resultadoXVR], [resultadoXVIL,resultadoXVIR], [resultadoXVIIL,resultadoXVIIR], [resultadoXVIIIL,resultadoXVIIIR],
+            [resultadoXIXL,resultadoXIXR], [resultadoXXL,resultadoXXR], [resultadoXXL,resultadoXXR], [resultadoXXIL,resultadoXXIR], [resultadoXXIIL,resultadoXXIIR], [resultadoXXIIIL,resultadoXXIIIR], [resultadoXXIVL,resultadoXXIVR], [resultadoXXVL,resultadoXXVR], [resultadoXXVIL,resultadoXXVIR],
+            [resultadoXXVIIL,resultadoXXVIIR], [resultadoXXVIIIL,resultadoXXVIIIR], [resultadoXXIXL,resultadoXXIXR], [resultadoXXXL,resultadoXXXR], [resultadoXXXIL,resultadoXXXIR], [resultadoXXXIIL,resultadoXXXIIR], [resultadoXXXIIIL,resultadoXXXIIIR], [resultadoXXXIVL,resultadoXXXIVR], [resultadoXXXVL,resultadoXXXVR],
+            [resultadoXXXVIL,resultadoXXXVIR], [resultadoXXXVIIL,resultadoXXXVIIR], [resultadoXXXVIIIL,resultadoXXXVIIIR], [resultadoXXXIXL,resultadoXXXIXR], [resultadoXLL,resultadoXLR], [resultadoXLIL,resultadoXLIR], [resultadoXLIIL,resultadoXLIIR], [resultadoXLIIIL,resultadoXLIIIR], [resultadoXLIVL,resultadoXLIVR],
+            [resultadoXLVL,resultadoXLVR], [resultadoXLVIL,resultadoXLVIR], [resultadoXLVIIL,resultadoXLVIIR], [resultadoXLVIIIL,resultadoXLVIIIR]]
+        })
+
+        console.log(informacionApuesta)
+        console.log(resultadoIL + resultadoIR);
+
+    const resultadoPartidos = [[0,2], [6,2], [0,2], [1,1], [1,2], [0,0], [0,0], [4,1], [0,0], [1,2], [7,0], [2,1], [2,1],
+         [2,1],  [2,1],  [2,1],  [2,1],  [2,1],  [2,1],  [2,1], [2,1], [2,1], [2,1], [2,1], [2,1], [2,1], [2,1], [2,1], 
+         [2,1],  [2,1],  [2,1],  [2,1],  [2,1],  [2,1],  [2,1], [2,1],  [2,1], [2,1], [2,1], [2,1], [2,1], [2,1], [2,1], [2,1],
+         [2,1],  [2,1],  [2,1],  [2,1]
+    ]
+
+
+    console.log(resultadoPartidos);
+    console.log(informacionApuesta[0].Partido1);
+
+        const puntosGanados = [];
+
+    for (let i = 0; i < resultadoPartidos.length; i++) {
+    if (resultadoPartidos[i][0] == informacionApuesta[0].Partido1[0] && resultadoPartidos[i][1] == informacionApuesta[0].Partido1[1]) {     //Caso 1: adivinar marcador
+        console.log('Felicidades, en este partido sumaste 3 puntos');        
+
+    } else if (resultadoPartidos[i].reduce((a,b)=> a-b) < 0 && informacionApuesta[0].Partido1.reduce((a,b) => a-b) < 0) {
+        console.log('Sumaste un punto al adivinar el ganador [Visitante]');
+
+    } else if (resultadoPartidos[i].reduce((a,b)=> a-b) > 0 && informacionApuesta[0].Partido1.reduce((a,b) => a-b) > 0) {
+        console.log('Sumaste un punto al adivinar el ganador [Local]');
+    
+    } else if (resultadoPartidos[i].reduce((a,b) => a-b) == 0 && informacionApuesta[0].Partido1.reduce((a,b) => a-b) == 0 && resultadoPartidos.encuentro1[0] !== informacionApuesta[0].Partido1[0]){
+        console.log('Sumaste un punto al adivinar el resultado [Empate]');
+    } else {
+        console.log('No ganaste puntos rey');
+    }
+}
+}
